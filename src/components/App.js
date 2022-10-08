07e8d0f1a1aa8e66a04e2ee4c6153617bde32a23
Harsh Vardhan <harsh.vardhan@accolite.com>
2022-10-09 00:32:43 +0530

    add code related youtube video app

---
+++ src/components/App.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    onTermSubmit("javascript");
+  }, []);
+
+  const onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
+  };
+
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList
+              onVideoSelect={(video) => setSelectedVideo(video)}
+              videos={videos}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+/*class Component*/
+// class App extends React.Component {
+//   state = { videos: [], selectedVideo: null };
+
+//   componentDidMount() {
+//     this.onTermSubmit("buildings");
+//   }
+
+//   onTermSubmit = async (term) => {
+//     const response = await youtube.get("/search", {
+//       params: {
+//         q: term,
+//       },
+//     });
+
+//     this.setState({
+//       videos: response.data.items,
+//       selectedVideo: response.data.items[0],
+//     });
+//   };
+
+//   onVideoSelect = (video) => {
+//     this.setState({ selectedVideo: video });
+//   };
+
+//   render() {
+//     return (
+//       <div className="ui container">
+//         <SearchBar onFormSubmit={this.onTermSubmit} />
+//         <div className="ui grid">
+//           <div className="ui row">
+//             <div className="eleven wide column">
+//               <VideoDetail video={this.state.selectedVideo} />
+//             </div>
+//             <div className="five wide column">
+//               <VideoList
+//                 onVideoSelect={this.onVideoSelect}
+//                 videos={this.state.videos}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
